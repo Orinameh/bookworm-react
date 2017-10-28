29837c9884be631e7c1c1d8ad3cc2a613bb16006
Orinameh <memefacto@yahoo.com>
2017-10-28 15:27:13 +0100

    added login and logout

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
+import { userLoggedIn } from "./actions/auth";
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+// makes the current user to be continually logged
+if (localStorage.bookwormJWT) {
+  const user = { token: localStorage.bookwormJWT};
+  store.dispatch(userLoggedIn(user))
+}
 
 ReactDOM.render(
   <BrowserRouter>
